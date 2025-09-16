c56f5aa691a21b302cf4122259f78ece791fba0c
mahir kambran <mahirkambran@gmail.com>
2025-09-16 12:03:54 +0530

    After allowed host change

--- hyd_realtors/settings.py
+++ hyd_realtors/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'koh1lie^h%vwuwdq9agc8h2u+-rsftvgffgkg*&jbu58x0fw*2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["realestate-website-with-django-4.onrender.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 
 RECAPTCHA_PUBLIC_KEY = '6Lf3MpwrAAAAAPsCl4n1SCVQnyTGrQlvpIJbkKED'
