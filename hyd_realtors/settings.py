b50d477c76de31884056b37f28f54f00a79450c6
mahir kambran <mahirkambran@gmail.com>
2025-09-16 12:38:21 +0530

    debug change

--- hyd_realtors/settings.py
+++ hyd_realtors/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'koh1lie^h%vwuwdq9agc8h2u+-rsftvgffgkg*&jbu58x0fw*2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["realestate-website-with-django-4.onrender.com"]
 
 
 RECAPTCHA_PUBLIC_KEY = '6Lf3MpwrAAAAAPsCl4n1SCVQnyTGrQlvpIJbkKED'
